ff6196c9efdc5dae1875d3becc272de4a90d5ebc
chris li <letmego163@163.com>
2016-01-19 23:50:56 +0800

    unity4threejs proj

---
+++ unity4threejs/unity4threejs/Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnButtonClick(string testCaseName) {
+		Debug.Log ("Button clicked" + testCaseName);
+	}
+}
